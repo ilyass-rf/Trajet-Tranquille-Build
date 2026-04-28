176da7e584b99bea2acd6e2b7ff286dbec66426a
💢プーティキャーティ <ilyasssalihi192@gmail.com>
2026-04-27 17:07:30 -0700

    Update database.py

--- vertion-developé/database.py
+++ vertion-developé/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, Float, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
-
+       
 SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/trajet_tranquille"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
