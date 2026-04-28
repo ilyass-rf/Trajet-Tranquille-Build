3b32360389652aca594e017da47da37e5378343a
Salihi-Ilyass-RF <ilyasssalihi192@gmail.com>
2026-04-28 01:06:03 -0700

    good job

---
+++ vertion-developé/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine, Column, Integer, Float, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import datetime
+
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/trajet_tranquille"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class BusLocation(Base):
+    __tablename__ = "bus_locations"
+    id = Column(Integer, primary_key=True, index=True)
+    bus_id = Column(String)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+
+Base.metadata.create_all(bind=engine)
